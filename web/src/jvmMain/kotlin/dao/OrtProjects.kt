2b6116fb88f5d1c05e22d3acc4c4d16d4a332416
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2020-04-17 17:53:41 +0200

    web: Add an inital database model for OrtProject
    OrtProject references a VCS repository and will be used as parent object
    for managing analysis, scans, evaluation and report generation for this
    repository.
    
    The DAO is created using the DAO API of Exposed [1].
    
    [1] https://github.com/JetBrains/Exposed/wiki/DAO
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@here.com>

---
+++ web/src/jvmMain/kotlin/dao/OrtProjects.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.web.jvm.dao
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Column
+
+object OrtProjects : IntIdTable() {
+    val name: Column<String> = text("name")
+    val type: Column<String> = text("type")
+    val url: Column<String> = text("url")
+    val path: Column<String> = text("path")
+}
+
+class OrtProjectDao(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<OrtProjectDao>(OrtProjects)
+
+    var name by OrtProjects.name
+    var type by OrtProjects.type
+    var url by OrtProjects.url
+    var path by OrtProjects.path
+}
